27da26b066df1c03b5fab5b66ccb8d66b4a12fb6
Michle99 <sammiyonyx@gmail.com>
2023-03-03 15:07:15 -0500

    Updated script to deployed render server

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://openai-server-ywbk.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
